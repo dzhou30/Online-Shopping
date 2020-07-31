58128a62efaca4033549f175ba8bd69a754f2386
davidzh30 <57373972+davidzh30@users.noreply.github.com>
2020-07-30 21:07:02 -0700

    Create ServerLogger.java

---
+++ src/main/java/onlineShop/log/ServerLogger.java
@@ -0,0 +1,13 @@
+package onlineShop.log;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "serverLogger")  //spring bean
+public class ServerLogger implements Logger {
+	
+	public void log(String info) {
+		System.out.println("server log = " + info);
+	}
+	
+}
+
